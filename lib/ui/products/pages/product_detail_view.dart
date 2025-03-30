1bf9adab5a0222d8840505b3dc48d02ea18d4fd5
felipepala1000 <felipepala1000@gmail.com>
2025-03-30 16:47:47 +0000

    Product detail screen

--- lib/ui/products/pages/product_detail_view.dart
+++ lib/ui/products/pages/product_detail_view.dart
@@ -1,16 +1,187 @@
 import 'package:flutter/material.dart';
 import 'package:myapp/models/product.dart';
 
-class ProductDetailView extends StatelessWidget {
+class ProductDetailView extends StatefulWidget {
   final Product product;
+
   const ProductDetailView({super.key, required this.product});
 
   @override
+  State<ProductDetailView> createState() => _ProductDetailViewState();
+}
+
+class _ProductDetailViewState extends State<ProductDetailView> {
+  int imageChoosed = 0;
+  void changeImage(int index) {
+    setState(() {
+      imageChoosed = index;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
+    Product product = widget.product;
     return Scaffold(
       backgroundColor: Colors.black,
-      appBar: AppBar(),
-      body: Center(child: Text('Product: ${product.title}')),
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        iconTheme: const IconThemeData(color: Colors.white),
+        title: const Text('Product', style: TextStyle(color: Colors.white)),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 2, horizontal: 10),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(10),
+              child: Image.network(
+                product.images[imageChoosed],
+                width: 300,
+                height: 290,
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          const SizedBox(height: 15),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: List.generate(product.images.length, (index) {
+              return GestureDetector(
+                onTap: () {
+                  changeImage(index);
+                },
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(7),
+                    child: Image.network(
+                      product.images[index],
+                      width: 65,
+                      height: 66,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+              );
+            }),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 15),
+            child: ProductTitlePrice(product: product),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 15),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  'Description',
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                    overflow: TextOverflow.ellipsis,
+                    leadingDistribution: TextLeadingDistribution.even,
+                  ),
+                ),
+                TextButton.icon(
+                  label: Text(
+                    'Show more',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  icon: Icon(
+                    Icons.arrow_drop_down_sharp,
+                    color: Colors.white,
+                    size: 24.0,
+                    semanticLabel: 'Show More',
+                  ),
+                  onPressed: () {
+                    showModalBottomSheet(
+                      backgroundColor: Colors.black,
+                      context: context,
+                      builder: (BuildContext context) {
+                        return Padding(
+                          padding: const EdgeInsets.symmetric(
+                            vertical: 8,
+                            horizontal: 15,
+                          ),
+                          child: SizedBox(
+                            height: 250,
+                            child: Center(
+                              child: Text(
+                                product.description,
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 15,
+                                ),
+                              ),
+                            ),
+                          ),
+                        );
+                      },
+                    );
+                  },
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            width: double.infinity,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 5.0),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color.fromRGBO(214, 243, 162, 1),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                ),
+                onPressed: () {},
+                child: Text(
+                  'Add to list',
+                  style: TextStyle(color: Colors.black, fontSize: 15),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ProductTitlePrice extends StatelessWidget {
+  const ProductTitlePrice({super.key, required this.product});
+
+  final Product product;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Text(
+          product.title,
+          style: const TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            fontSize: 18,
+            overflow: TextOverflow.ellipsis,
+            leadingDistribution: TextLeadingDistribution.even,
+          ),
+        ),
+        Text(
+          '\$ ${product.price.toString()}',
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            fontSize: 20,
+          ),
+        ),
+      ],
     );
   }
 }
